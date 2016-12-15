81e236c87b5e91a6b51c0a3771ecd70a406b7c75
Jordan Sissel <jls@semicomplete.com>
2016-12-14 16:15:52 -0800

    Add hack library to make libsodium build and link

---
+++ lib/fakeposix/extra.h
@@ -0,0 +1,21 @@
+#ifndef _EXTRA_H_
+#define _EXTRA_H_
+typedef int ssize_t;
+
+#define O_RDONLY 0
+
+// Maybe this hack is sufficient.
+#define S_ISCHR(x) 0
+
+struct stat { 
+  int st_mode;
+};
+
+struct timeval { 
+  int tv_sec;
+  int tv_usec;
+};
+
+extern int close(int fd);
+extern int open(const char *pathname, int flags);
+#endif /* _EXTRA_H_ */
---
+++ lib/fakeposix/functions.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+
+extern "C" {
+  #include "extra.h"
+  int close(int fd) {
+    return 0;
+  }
+
+  int open(const char *pathname, int flags) {
+    Serial.print("open(");
+    Serial.print(pathname),
+    Serial.println("");
+    return 0;
+  }
+
+  int fstat(int fd, struct stat *buf) {
+    return 0;
+  }
+
+  ssize_t read(int fd, void *buf, size_t count) {
+    return 0;
+  }
+}
