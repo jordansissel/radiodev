bcfa233380a5c25ef910985a9fde6917e3749f93
Jordan Sissel <jls@semicomplete.com>
2016-12-14 16:16:25 -0800

    Trying to make libsodium available...

--- receiver/src/main.ino
+++ receiver/src/main.ino
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <RH_ASK.h>
-#include "KeyInstruction.h"
-#include "Instructions.h"
+#include "sodium.h"
 
 #define rxled 17
 #define rxpin 15
@@ -18,14 +17,40 @@ void loop() {
   uint8_t buflen = sizeof(buf);
   if (driver.recv(buf, &buflen)) {
     driver.printBuffer("Got:", buf, buflen);
-
-    switch (static_cast<Instructions>(buf[0])) {
-    case Instructions::KeyInstruction:
-      Serial.println("Got a key instruction");
-      break;
-    default:
-      Serial.println("Unknown instruction.");
-      break;
-    }
   }
+
+  x();
+  delay(1000);
+}
+
+#define MESSAGE (const unsigned char *) "test"
+#define MESSAGE_LEN 4
+#define CIPHERTEXT_LEN (crypto_box_MACBYTES + MESSAGE_LEN)
+void x() {
+  unsigned char alice_publickey[crypto_box_PUBLICKEYBYTES];
+  unsigned char alice_secretkey[crypto_box_SECRETKEYBYTES];
+  crypto_box_keypair(alice_publickey, alice_secretkey);
+
+  unsigned char bob_publickey[crypto_box_PUBLICKEYBYTES];
+  unsigned char bob_secretkey[crypto_box_SECRETKEYBYTES];
+  crypto_box_keypair(bob_publickey, bob_secretkey);
+
+  unsigned char nonce[crypto_box_NONCEBYTES];
+  unsigned char ciphertext[CIPHERTEXT_LEN];
+  randombytes_buf(nonce, sizeof nonce);
+  Serial.print("Plain text: "); Serial.write((const uint8_t*) MESSAGE, MESSAGE_LEN); Serial.println();
+
+  if (crypto_box_easy(ciphertext, MESSAGE, MESSAGE_LEN, nonce,
+                      bob_publickey, alice_secretkey) != 0) {
+    /* error */
+  }
+
+  Serial.print("Ciphertext : "); Serial.write((const uint8_t*) ciphertext, CIPHERTEXT_LEN); Serial.println();
+
+  unsigned char decrypted[MESSAGE_LEN];
+  if (crypto_box_open_easy(decrypted, ciphertext, CIPHERTEXT_LEN, nonce,
+                           alice_publickey, bob_secretkey) != 0) {
+    /* message for Bob pretending to be from Alice has been forged! */
+  }
+  Serial.print("Decrypted text: "); Serial.write((const uint8_t*) decrypted, MESSAGE_LEN); Serial.println();
 }
