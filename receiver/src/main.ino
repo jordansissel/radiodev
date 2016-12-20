a3a9791a06a3c61f2b4f3219845cb55ffff3b6ff
Jordan Sissel <jls@semicomplete.com>
2016-12-19 22:31:00 -0800

    Try secretbox on a 32u4

--- receiver/src/main.ino
+++ receiver/src/main.ino
@@ -8,7 +8,7 @@ RH_ASK driver(2000, rxpin, 1, 2);
 
 void setup() {
   Serial.begin(9600); // Debugging only
-  if (!driver.init())
+  //if (!driver.init())
   Serial.println("init failed");
   Serial.print("Setup complete.");
 }
@@ -19,14 +19,15 @@ void loop() {
     driver.printBuffer("Got:", buf, buflen);
   }
 
-  x();
+  y();
   delay(1000);
 }
 
-#define MESSAGE (const unsigned char *) "test"
-#define MESSAGE_LEN 4
+#define MESSAGE (const unsigned char *) "hello world. everything is on fire."
+#define MESSAGE_LEN 35
 #define CIPHERTEXT_LEN (crypto_box_MACBYTES + MESSAGE_LEN)
 void x() {
+  unsigned long start;
   unsigned char alice_publickey[crypto_box_PUBLICKEYBYTES];
   unsigned char alice_secretkey[crypto_box_SECRETKEYBYTES];
   crypto_box_keypair(alice_publickey, alice_secretkey);
@@ -40,17 +41,52 @@ void x() {
   randombytes_buf(nonce, sizeof nonce);
   Serial.print("Plain text: "); Serial.write((const uint8_t*) MESSAGE, MESSAGE_LEN); Serial.println();
 
+  start = micros();
   if (crypto_box_easy(ciphertext, MESSAGE, MESSAGE_LEN, nonce,
                       bob_publickey, alice_secretkey) != 0) {
-    /* error */
+    Serial.println("crypto_box_easy: ERROR!?");
   }
+  Serial.print("crypto_box_easy Duration: ");
+  Serial.print(micros() - start);
+  Serial.println("us");
 
   Serial.print("Ciphertext : "); Serial.write((const uint8_t*) ciphertext, CIPHERTEXT_LEN); Serial.println();
 
   unsigned char decrypted[MESSAGE_LEN];
+  start = micros();
   if (crypto_box_open_easy(decrypted, ciphertext, CIPHERTEXT_LEN, nonce,
                            alice_publickey, bob_secretkey) != 0) {
-    /* message for Bob pretending to be from Alice has been forged! */
+    Serial.println("crypto_box_open_easy FORGERY OR CORRUPTION");
   }
+  Serial.print("crypto_box_open_easy Duration: ");
+  Serial.print(micros() - start);
+  Serial.println("us");
   Serial.print("Decrypted text: "); Serial.write((const uint8_t*) decrypted, MESSAGE_LEN); Serial.println();
+}
+
+void y() {
+  unsigned int ciphertext_len = (crypto_secretbox_MACBYTES + MESSAGE_LEN);
+  unsigned long start;
+
+  unsigned char nonce[crypto_secretbox_NONCEBYTES];
+  unsigned char key[crypto_secretbox_KEYBYTES];
+  unsigned char ciphertext[CIPHERTEXT_LEN];
+
+  randombytes_buf(nonce, sizeof nonce);
+  randombytes_buf(key, sizeof key);
+
+  start = micros();
+  crypto_secretbox_easy(ciphertext, MESSAGE, MESSAGE_LEN, nonce, key);
+  Serial.print("crypto_secretbox_easy Duration: ");
+  Serial.print(micros() - start);
+  Serial.println("us");
+
+  unsigned char decrypted[MESSAGE_LEN];
+  start = micros();
+  if (crypto_secretbox_open_easy(decrypted, ciphertext, CIPHERTEXT_LEN, nonce, key) != 0) {
+    Serial.println("crypto_secretbox_open_easy FORGED OR CORRUPT");
+  }
+  Serial.print("crypto_secretbox_open_easy Duration: ");
+  Serial.print(micros() - start);
+  Serial.println("us");
 }
