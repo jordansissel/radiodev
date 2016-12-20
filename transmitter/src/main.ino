a2cc1eae567aed248a731734e4a83db3d264781d
Jordan Sissel <jls@semicomplete.com>
2016-12-19 21:30:12 -0800

    Fiddling with libsodium

--- transmitter/src/main.ino
+++ transmitter/src/main.ino
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <RH_ASK.h>
-#include "KeyInstruction.h"
-#include "Instructions.h"
+#include "sodium.h"
+//#include "KeyInstruction.h"
+//#include "Instructions.h"
 
 #define txpin 5  //RF Transmitter pin = digital pin 4
 #define txled 1        //Onboard LED = digital pin 13
@@ -16,21 +17,54 @@ void setup(){
   }
 
   Serial.println("1");
-  KeyInstruction k(0x65);
+  //KeyInstruction k(0x65);
   Serial.println("2");
   uint32_t count;
   Serial.println("3");
   uint8_t buf[10];
   Serial.println("4");
-  count = k.serialize(buf, 10);
+  //count = k.serialize(buf, 10);
   Serial.println("5");
   driver.send(buf, count);
   Serial.println("6");
   driver.waitPacketSent();
   Serial.println("7");
   Serial.println("Sent.");
+  x();
 }
 
 void loop(){
   delay(2000);
+}
+
+#define MESSAGE (const unsigned char *) "test"
+#define MESSAGE_LEN 4
+#define CIPHERTEXT_LEN (crypto_box_MACBYTES + MESSAGE_LEN)
+void x() {
+  unsigned char alice_publickey[crypto_box_PUBLICKEYBYTES];
+  unsigned char alice_secretkey[crypto_box_SECRETKEYBYTES];
+  crypto_box_keypair(alice_publickey, alice_secretkey);
+
+  unsigned char bob_publickey[crypto_box_PUBLICKEYBYTES];
+  unsigned char bob_secretkey[crypto_box_SECRETKEYBYTES];
+  crypto_box_keypair(bob_publickey, bob_secretkey);
+
+  unsigned char nonce[crypto_box_NONCEBYTES];
+  unsigned char ciphertext[CIPHERTEXT_LEN];
+  randombytes_buf(nonce, sizeof nonce);
+  Serial.print("Plain text: "); Serial.write((const uint8_t*) MESSAGE, MESSAGE_LEN); Serial.println();
+
+  if (crypto_box_easy(ciphertext, MESSAGE, MESSAGE_LEN, nonce,
+                      bob_publickey, alice_secretkey) != 0) {
+    /* error */
+  }
+
+  Serial.print("Ciphertext : "); Serial.write((const uint8_t*) ciphertext, CIPHERTEXT_LEN); Serial.println();
+
+  unsigned char decrypted[MESSAGE_LEN];
+  if (crypto_box_open_easy(decrypted, ciphertext, CIPHERTEXT_LEN, nonce,
+                           alice_publickey, bob_secretkey) != 0) {
+    /* message for Bob pretending to be from Alice has been forged! */
+  }
+  Serial.print("Decrypted text: "); Serial.write((const uint8_t*) decrypted, MESSAGE_LEN); Serial.println();
 }
