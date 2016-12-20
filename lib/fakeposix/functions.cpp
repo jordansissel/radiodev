f9ffef112b8c22c87056a9c4b05233b5dec35709
Jordan Sissel <jls@semicomplete.com>
2016-12-19 21:40:29 -0800

    More posix prints

--- lib/fakeposix/functions.cpp
+++ lib/fakeposix/functions.cpp
@@ -10,15 +10,23 @@ extern "C" {
   int open(const char *pathname, int flags, ...) {
     Serial.print("open(");
     Serial.print(pathname),
-    Serial.println("");
+    Serial.println(")");
     return 0;
   }
 
   int fstat(int fd, struct stat *buf) {
+    Serial.print("fstat(");
+    Serial.print(fd);
+    Serial.println(")");
     return 0;
   }
 
   ssize_t read(int fd, void *buf, size_t count) {
+    Serial.print("read(");
+    Serial.print(fd);
+    Serial.print(", ");
+    Serial.print(count);
+    Serial.println(")");
     return 0;
   }
 }
